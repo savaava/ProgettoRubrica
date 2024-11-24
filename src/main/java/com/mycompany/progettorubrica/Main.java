b2d220a61a3d9a73045604b4746380d624df10ef
annamariaScermino <annamaria180903@gmail.com>
2024-11-24 15:54:27 +0100

    Revert "secondo commit"
    This reverts commit e0d1679131e9ab4f4633eee01629183ac2074e77.

--- src/main/java/com/mycompany/progettorubrica/Main.java
+++ src/main/java/com/mycompany/progettorubrica/Main.java
@@ -3,6 +3,5 @@ package com.mycompany.progettorubrica;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello woorld!!");
-        System.out.println("ciao");
     }
 }
