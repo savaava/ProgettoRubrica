e0d1679131e9ab4f4633eee01629183ac2074e77
annamariaScermino <annamaria180903@gmail.com>
2024-11-24 15:37:15 +0100

    secondo commit

--- src/main/java/com/mycompany/progettorubrica/Main.java
+++ src/main/java/com/mycompany/progettorubrica/Main.java
@@ -3,5 +3,6 @@ package com.mycompany.progettorubrica;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello woorld!!");
+        System.out.println("ciao");
     }
 }
