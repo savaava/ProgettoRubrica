4cce935ee592b2193c2ee3b83bd1e238bd70b653
Alessandro Monte <ale.monte8888@gmail.com>
2024-12-06 09:32:41 +0100

    implementazione scheletro

---
+++ src/main/java/gruppo1/progettorubrica/controllers/ManageTagsPopupController.java
@@ -0,0 +1,37 @@
+package gruppo1.progettorubrica.controllers;
+
+import gruppo1.progettorubrica.models.AddressBook;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManageTagsPopupController implements Initializable {
+    private AddressBook addressBook;
+
+    @FXML
+    private Button addButton, updateButton, deleteButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void onAddButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onUpdateButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onDeleteButtonPressed(ActionEvent event) {
+
+    }
+}
--- src/main/java/gruppo1/progettorubrica/services/MongoSerializable.java
+++ src/main/java/gruppo1/progettorubrica/services/MongoSerializable.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gruppo1.progettorubrica.services;
 
-/**
- *
- * @author franz
- */
-public interface MongoSerializable {
-    
+import org.bson.Document;
+
+public interface MongoSerializable<T> {
+    Document toDocument();
+    T fromDocument(Document document);
 }
