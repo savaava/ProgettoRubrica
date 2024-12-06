29fb994e4b64214d501aa97c4ecf8ed6e9f8d032
Annamaria Scermino <a.scermino7@studenti.unisa.it>
2024-12-06 10:07:25 +0100

    Documentazione ImagePopupController

--- src/main/java/gruppo1/progettorubrica/controllers/ImagePopupController.java
+++ src/main/java/gruppo1/progettorubrica/controllers/ImagePopupController.java
@@ -5,10 +5,60 @@
  */
 package gruppo1.progettorubrica.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
- *
- * @author franz
+ * @brief Controller che visualizza il popup contenente immagini contatto.
+ * 
+ * In tale popup, l'utente può associare un'immagine ad un contatto, selezionandola dalle immagini suggerite o caricarne una personalizzata.
  */
-public class ImagePopupController {
+
+public class ImagePopupController implements Initializable {
+
+    @FXML
+    private ImageView imgAdd; ///< pulsante per poter caricare un'immagine personalizzata
+
+    @FXML
+    private HBox defaultImgHBox; ///< contiene le immagini suggerite
+
+    private File selectedImage; ///< file dell'immagine caricata dall'utente
+    
+    private int imageIndex; ///< indice dell'immagine scelta dall'utente
+
+
+    /**
+     * @brief Carica l'image popup controller.
+     * 
+     * Questo metodo permette di visualizzare il popup image quando l'utente clicca sull'immagine di un contatto.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    /**
+     * @return Il file dell'immagine caricata dall'utente.
+     * 
+     */
+    public File getSelectedImage() {
+        return this.selectedImage;
+    }
+
+    /**
+     * @brief Indica se l'utente ha scelto un'immagine tra quelle suggerite o se ne ha caricata una personalizzata.
+     * @return L'indice (compreso tra 1 e 4) associato all'immagine selezionata, altrimenti l'indice=5 se l'utente ha caricato un'immagine personalizzata.
+     */
+    public int getImageIndex() {
+        return this.imageIndex;
+    }
+    
     
 }
+
