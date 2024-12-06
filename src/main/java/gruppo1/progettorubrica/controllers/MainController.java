befcc173d2aca04cea75f81fdc18c4d5b41c4ea7
Francesco Musto <cicciobacco2014@gmail.com>
2024-12-06 09:14:56 +0100

    Creazione struttura progetto

---
+++ src/main/java/gruppo1/progettorubrica/controllers/MainController.java
@@ -0,0 +1,261 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppo1.progettorubrica.controllers;
+
+import javafx.collections.transformation.FilteredList;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import gruppo1.progettorubrica.models.AddressBook;
+import gruppo1.progettorubrica.models.Contact;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @brief Controller che si occupa della scena iniziale.
+ * 
+ * In tale scena è possibile aggiungere, rimuovere, modificare e cercare contatti.
+ * Inoltre è possibile aggiungere un'immagine profilo ad un contatto.
+ */
+
+public class MainController implements Initializable {
+    private AddressBook addressBook;
+
+    @FXML
+    private Button deleteButton, saveButton, editButton, cancelButton;
+
+    @FXML
+    private VBox tagVBox, lettersVBox;
+
+    @FXML
+    private TextField searchField, nameField, surnameField, emailField, numberField;
+
+    @FXML
+    private ImageView profileImageView, filterImage;
+
+    @FXML
+    private TableView<Contact> contactsTable;
+
+    @FXML
+    private TableColumn<Contact, String> nameColumn,surnameColumn;
+
+    @FXML
+    private AnchorPane contactDetailsPane;
+
+    @FXML
+    private GridPane numbersPane, emailsPane;
+
+    private ContextMenu contextMenu;
+
+    private FilteredList<Contact> filteredContacts;
+
+    /**
+     * @brief Inizializza il main controller
+     *
+     *  Questo metodo permette l'inizializzazione del controller in fase di apertura.
+     *
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.addressBook = AddressBook.getInstance();
+    }
+
+    /**
+     * @brief Imposta la foto profilo circolare
+     *
+     *  Questo metodo permette di avere la foto profilo di un contatto circolare (di default la foto è di forma rettangolare).
+     *
+     */
+    private void setImageCircle() {
+
+    }
+
+    /**
+     * @brief Aggiorna l'elenco dei contatti
+     *
+     *  Questo metodo si occupa di svolgere il binding che permette l'aggiornamento della listView in base ai parametri di ricerca.
+     */
+    private void searchFieldBinding() {
+
+    }
+
+    /**
+     * @brief Gestisce l'evento di cliccare sull'icona di filtraggio
+     *
+     * Questo metodo permette, al verificarsi del mouseEvent, di aprire il menù contestuale di gestione dei tags.
+     *
+     * @param[in] mouseEvent
+     */
+    @FXML
+    public void onFilterIconClicked(MouseEvent mouseEvent) {
+
+    }
+
+    /**
+     * @brief Aggiunge contatto
+     *
+     * Questo metodo per l'aggiunta del contatto viene lanciato sia dal pulsante + che da "Crea contatto" nel menù contestuale.
+     *
+     * @param[in] event
+     */
+    @FXML
+    private void onAddButtonPressed(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Vista dettagliata del contatto
+     *
+     * Questo metodo permette, al verificarsi dell'evento, di poter rendere visibile la visione dettagliata del contatto scelto.
+     *
+     * @param[in] event
+     */
+    @FXML
+    private void onContactClicked(MouseEvent event) {
+
+    }
+
+    /**
+     * @brief Modifica contatto
+     *
+     * Questo metodo permette, una volta cliccato il pulsante "Modifica", di modificare il contatto in visione dettagliata.
+     *
+     * @param[in] event
+     */
+    @FXML
+    private void onModifyContact(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Cancella contatto
+     *
+     * Tramite questo metodo, cliccando il pulsante "Elimina" dalla visione dettagliata del contatto scelto, possiamo eliminarlo.
+     *
+     * @param[in] event
+     */
+    @FXML
+    private void onDeleteContact(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Salva contatto
+     *
+     * Guarda anche: onModifyContact()
+     * Questo metodo permette, una volta cliccato il pulsante "Salva", di modificare/creare il contatto in visione dettagliata.
+     *
+     * @param[in] event
+     * @see onModifyContact()
+     */
+    @FXML
+    private void onSaveContact(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Annulla modifiche contatto
+     *
+     * Guarda anche: onModifyContact()
+     * Questo metodo permette, una volta cliccato il pulsante "Annulla", di annullare la modifica/creazione di un contatto in visione dettagliata.
+     *
+     * @param[in] event
+     * @see onModifyContact()
+     */
+    @FXML
+    private void onCancelContact(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Apre popup di import
+     *
+     * Guarda anche ImportPopupController
+     * Questo metodo permette, una volta cliccato sulla voce "Importa" del menù FILE a tendina, di aprire il popup per l'import dei contatti.
+     *
+     * @param[in] event
+     * @see ImportPopupController
+     */
+    @FXML
+    private void showImportPopup(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Apre popup di export
+     *
+     * Guarda anche ExportPopupController
+     * Questo metodo permette, una volta cliccato sulla voce "Esporta" del menù FILE a tendina, di aprire il popup per l'export dei contatti.
+     *
+     * @param[in] event
+     * @see ExportPopupController
+     */
+    @FXML
+    private void showExportPopup(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Apre popup di collegamento al database
+     *
+     * Tramite questo metodo, una volta cliccato sulla voce "Configurazione" del menù a tendina FILE, apriamo il popup per aggiungere il database su cui memorizzare i contatti.
+     *
+     *
+     * @param[in] event
+     */
+    @FXML
+    private void showConfigPopup(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Ottengo le informazioni sulla foto profilo del contatto
+     *
+     * Guarda anche ImagePopupController
+     *
+     * Questo metodo, una volta ottenuto il controller tramite il metodo getController(), utilizza
+     * i metodi getSelectedImage() e getImageIndex per ottenere le informazioni riguardo l'immagine profilo d'interesse.
+     *
+     * @param[in] event
+     * @see ImagePopupController
+     */
+    @FXML
+    private void showImagePopup(ActionEvent event) {
+        //Bisogna ottenere il controller tramite il metodo getController()
+        //Successivamente bisogna usare i metodi getSelectedImage() e getImageIndex()
+    }
+
+    /**
+     * @brief Mostra il popup di gestione dei tag
+     *
+     * Guarda anche ManageTagsPopupController
+     * Questo metodo si occupa della gestione dei tag.
+     *
+     * @param[in] event
+     * @see ManageTagsPopupController
+     */
+    @FXML
+    private void showManageTagsPopup(ActionEvent event) {
+
+    }
+
+    /**
+     * @brief Crea menù contestuale
+     *
+     * Questo metodo, eseguito solo in fase di inizializzazione della rubrica, crea il menù contestuale
+     * @return Il menù contestuale
+     */
+    private ContextMenu createContextMenu(){
+        return null;
+    }
+}
---
+++ src/main/java/gruppo1/progettorubrica/services/MongoSerializable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppo1.progettorubrica.services;
+
+/**
+ *
+ * @author franz
+ */
+public interface MongoSerializable {
+    
+}
