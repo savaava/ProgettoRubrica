c14d4e26ebb731f2c9f60b76412c50cf2fd1ef24
Francesco Musto <cicciobacco2014@gmail.com>
2024-12-06 09:27:06 +0100

    Implementazione AddressBookMain e ExportPopupController

---
+++ src/main/java/gruppo1/progettorubrica/AddressBookMain.java
@@ -0,0 +1,16 @@
+package gruppo1.progettorubrica;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddressBookMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- src/main/java/gruppo1/progettorubrica/controllers/ExportPopupController.java
+++ src/main/java/gruppo1/progettorubrica/controllers/ExportPopupController.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gruppo1.progettorubrica.controllers;
 
-/**
- *
- * @author franz
- */
-public class ExportPopupController {
-    
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import gruppo1.progettorubrica.models.AddressBook;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ExportPopupController implements Initializable {
+    private AddressBook addressBook;
+
+    @FXML
+    private ChoiceBox<String> exportChoiceBox;
+
+    @FXML
+    private RadioButton vCardButton, csvButton;
+
+    @FXML
+    private ToggleGroup ext;
+
+    @FXML
+    private Button saveButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.addressBook = AddressBook.getInstance();
+    }
+
+    @FXML
+    private void choosePath(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onExportButtonPressed(ActionEvent event) {
+
+    }
 }
