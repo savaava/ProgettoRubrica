820b842162df285643e82e32917c8047d0d15d7a
Alessandro Monte <ale.monte8888@gmail.com>
2024-12-06 09:58:26 +0100

    modifiche views e controllers

---
+++ src/main/java/gruppo1/progettorubrica/controllers/ConfirmPopupController.java
@@ -0,0 +1,16 @@
+package gruppo1.progettorubrica.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ConfirmPopupController {
+    @FXML
+    private void onConfirm(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onCancel(ActionEvent event) {
+
+    }
+}
