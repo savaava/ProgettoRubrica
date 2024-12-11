79a266e2e6a415ac76d861806bb7fac35fbccb25
Annamaria Scermino <a.scermino7@studenti.unisa.it>
2024-12-11 16:53:22 +0100

    implementazione ManageTagPopupController

--- src/main/java/gruppo1/progettorubrica/controllers/ManageTagsPopupController.java
+++ src/main/java/gruppo1/progettorubrica/controllers/ManageTagsPopupController.java
@@ -14,6 +14,7 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * @brief Controller che visualizza il popup dei tag.
@@ -81,5 +82,9 @@ public class ManageTagsPopupController implements Initializable {
     @FXML
     private void onDelete(ActionEvent event) {
         
+    }
+
+    @FXML
+    private void onTagClicked(MouseEvent event) {
     }
 }
